d02255969b9b359c32586f32c883224b1b049d65
rohan-fowlerharper <rohan.fowlerharper@gmail.com>
2022-04-04 20:48:07 +1200

    add solution

---
+++ packages/leetcode/src/first-bad-version/solution.js
@@ -0,0 +1,33 @@
+/**
+ * Definition for isBadVersion()
+ *
+ * @param {integer} version number
+ * @return {boolean} whether the version is bad
+ * isBadVersion = function(version) {
+ *     ...
+ * };
+ */
+
+/**
+ * @param {function} isBadVersion()
+ * @return {function}
+ */
+let solution = (isBadVersion) => {
+  /**
+   * @param {integer} n Total versions
+   * @return {integer} The first bad version
+   */
+  return (n) => {
+    let left = 1
+    let right = n
+    while (left < right) {
+      let mid = Math.floor((left + right) / 2)
+      if (isBadVersion(mid)) {
+        right = mid
+      } else {
+        left = mid + 1
+      }
+    }
+    return left
+  }
+}
