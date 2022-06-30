6ff9d15fe915d9064aa5d7582a2c99f0bc4006cf
Rohan Fowler-Harper <rohan.fowlerharper@gmail.com>
2022-06-30 20:14:19 +1200

    add solution

---
+++ leetcode/0003_longest-substring-without-repeating-characters/solution.ts
@@ -0,0 +1,22 @@
+function lengthOfLongestSubstring(s: string): number {
+  if (!s) {
+    return 0
+  }
+  let start = 0
+  let end = 0
+  let maxLength = 0
+  const uniqueCharacters = new Set<string>()
+  while (end < s.length) {
+    if (!uniqueCharacters.has(s[end])) {
+      uniqueCharacters.add(s[end])
+      end++
+      maxLength = Math.max(maxLength, uniqueCharacters.size)
+    } else {
+      uniqueCharacters.delete(s[start])
+      start++
+    }
+  }
+  return maxLength
+}
+
+console.log(lengthOfLongestSubstring('abcabcbb'))
